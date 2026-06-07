55618506791db41b6ec27c0144e05df4e895e790
ToyosiOgundeyi <ogundeyi@uab.edu>
2026-06-07 17:58:00 +0000

    Lab 1 prime number checker

---
+++ Labs/prime.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    int given_number;
+    int i;
+
+    printf("Enter a number: ");
+    scanf("%d", &given_number);
+
+    if (given_number <= 1)
+    {
+        printf("The number is not prime\n");
+    }
+    else
+    {
+        for (i = 2; i < given_number; i++)
+        {
+            if (given_number % i == 0)
+            {
+                printf("The number is not prime\n");
+                return 0;
+            }
+        }
+
+        printf("The number is prime\n");
+    }
+
+    return 0;
+}
